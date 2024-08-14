8574bddb23c8789cb5ef5f4605ee7397042552a6
Gauatm-raj <153638242+Gauatm-raj@users.noreply.github.com>
2024-08-14 23:14:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
-  // write your code here
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < l; i++) {
+        const randomIndex = Math.floor(Math.random() * charactersLength);
+        result += characters[randomIndex];
+    }
+    return result;
 }
 
 // Do not change the code below.
